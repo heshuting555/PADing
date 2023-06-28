befb3af050c6490151685bf7429760150e7cd78a
Shuting He <41313965+heshuting555@users.noreply.github.com>
2023-06-28 19:51:14 +0800

    Update __init__.py

--- PADing/data/datasets/__init__.py
+++ PADing/data/datasets/__init__.py
@@ -2,7 +2,6 @@
 from . import (
 register_coco_stuff,
 register_coco_zsi_48_17,
-# register_coco_zsi,
-# register_coco_zsi_test,
-register_coco_panoptic_zsp
+register_coco_panoptic_zsp,
+register_coco_zsi_65_15
 )
